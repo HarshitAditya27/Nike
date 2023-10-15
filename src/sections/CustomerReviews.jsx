45f71126e3d6096ecdb1e32c03a92f1fdfb5fe12
Harshit Aditya <harshitaditya27@gmail.com>
2023-10-16 01:51:45 +0530

    Added Customer review and subscribe section

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,3 +1,27 @@
+import ReviewCard from "../components/ReviewCard";
+import { reviews } from "../constants";
+
 export default function CustomerReviews() {
-  return <div>CustomerReviews</div>;
+  return (
+    <section className="max-container">
+      <h3 className="font-planquin text-center text-4xl font-bold">
+        What our <span className="text-coral-red ">Customers</span> say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Hear genuine stories from our satisfied customers abot their exceptional
+        experience with us.
+      </p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewCard
+            key={review.customerName}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
